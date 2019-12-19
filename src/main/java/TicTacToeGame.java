575e57aec1752688dfe95304f93a746fa8750c9e
Matthew Wiewel <mjvwiewel@gmail.com>
2019-12-18 18:40:37 -0600

    correct import for ImageView class from java swing to java fx

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -2,13 +2,12 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import javax.swing.text.html.ImageView;
 
 public class TicTacToeGame extends Application
 {
