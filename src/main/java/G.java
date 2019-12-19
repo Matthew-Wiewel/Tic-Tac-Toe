3780d6390510038b0b524d4c1363f284776098aa
Matthew Wiewel <mjvwiewel@gmail.com>
2019-12-18 18:29:16 -0600

    made N no longer final

--- src/main/java/G.java
+++ src/main/java/G.java
@@ -1,7 +1,7 @@
 //used for storing global constants, G for Global
 class G
 {
-    static final int N = 3; //determines boards size
+    static int N = 3; //determines boards size
     static final int BLANK = 0; //denotes blank squares on board or neither player for win purposes
     static final int X = 1; //denotes squares with X or a win for player X
     static final int O = -1; //denotes squares with an O or a win for player O (note zero is 0 in this font)
