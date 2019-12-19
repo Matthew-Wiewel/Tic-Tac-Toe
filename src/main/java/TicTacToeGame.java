70ba907031a3de49b1a6f3c807f06eb3ed31b00c
Matthew Wiewel <mjvwiewel@gmail.com>
2019-12-18 18:29:32 -0600

    added basic skeleton for GUI

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,8 +1,66 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
 
 public class TicTacToeGame extends Application
 {
+    private Stage primaryStage;
+    private int boardSize;
+    private int numWon;
+    private int numDrawn;
+    private int numLost;
+
+    private Scene homeScene; //scene for selecting difficulty, playing, and seeing history
+    private HBox homeSceneBox;
+    private MenuBar topMenu; //used for menu to go across top of the screen
+    private Menu options; //menu to hold various options for quitting or clearing history
+    private MenuItem freshStart;
+    private MenuItem quit;
+    private Menu boardSizeOptions; //menu to select board size
+    private MenuItem size3x3;
+    private MenuItem size4x4;
+    private MenuItem size5x5;
+    private Menu difficultyOptions; //menu to select difficulty
+    private MenuItem selectEasy;
+    private MenuItem selectMedium;
+    private MenuItem selectHard;
+    private MenuItem selectExpert;
+    private VBox selectionAndPlayHolder; //used to hold displays of current selections and play button
+    private TextField currentBoardSizeDisplay;
+    private TextField currentDifficultyDisplay;
+    private Button playButton;
+
+    private Scene playScene; //used for playing the game of Tic Tac Toe
+    private BorderPane playBox; //used to hold the GUI in this scene
+    private TextField mostRecentMoveDisplay;
+    private GridPane board; //will hold the squares of the tic tac toe board
+    private ImageView[][] boardImages; //will have clickable images for making moves
+    private HBox bottomInfo; //will hold nodes at bottom of border pane
+    private Button backToHome;
+    private TextField whoWonDisplay;
+    private Button exitGame; //used to quit game in progress
+    private Button confirmExitGame; //used to save user from misclicks mid-game
+
+    private void remakeBoard()
+    {
+        //TODO, re-make the board each time the size is changed. Do not need to remake the entire play scene
+    }
+
+    private void createScenes()
+    {
+        //TODO, make home scene
+
+        //TODO, make play scene
+        remakeBoard(); //will initially be for the 3x3
+    }
+
     //main function to allow mvn exec:main and mvn compile to launch program with the given plugins
     public static void main(String[] args)
     {
@@ -13,6 +71,7 @@ public class TicTacToeGame extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        this.primaryStage = primaryStage; //set equal so we can reference the primary stage outside of start
         //TODO: add GUI
     }
 
