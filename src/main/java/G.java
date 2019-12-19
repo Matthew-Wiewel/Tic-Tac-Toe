9a5ce26b8a9eeb882bbd35fa78ad6643f4250c1f
Matthew Wiewel <mjvwiewel@gmail.com>
2019-12-19 12:22:27 -0600

    added toString method for the constants

--- src/main/java/G.java
+++ src/main/java/G.java
@@ -5,4 +5,22 @@ class G
     static final int BLANK = 0; //denotes blank squares on board or neither player for win purposes
     static final int X = 1; //denotes squares with X or a win for player X
     static final int O = -1; //denotes squares with an O or a win for player O (note zero is 0 in this font)
+    static final int DRAW = 2;
+
+    static String toString(int num)
+    {
+        switch(num)
+        {
+            case BLANK:
+                return "Blank";
+            case X:
+                return "X";
+            case O:
+                return "O";
+            case DRAW:
+                return "Draw";
+            default:
+                return null;
+        }
+    }
 }
